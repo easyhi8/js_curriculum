a46628ec1f2842faab703b37af6fe19b55a1e92b
h.iijima <easy.s2m@gmail.com>
2024-10-02 16:27:54 +0900

    feat:db.jsファイルの作成

--- public/js/db.js
+++ public/js/db.js
@@ -1,15 +1,12 @@
-// db.js
 const mysql = require('mysql');
 
-// MySQLデータベースへの接続設定
 const connection = mysql.createConnection({
-    host: 'localhost', // MySQLサーバーのホスト名
-    user: 'root', // MySQLのユーザー名
-    password: 'rootroot', // MySQLのパスワード
-    database: 'sampple_db' // 使用するデータベース名名前は任せます
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'sampple_db'
 });
 
-// データベースへの接続
 connection.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL database:', err);
